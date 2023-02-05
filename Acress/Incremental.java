c359b0a2590785c2ac0ff228a8c39ec2e40495c0
Lucas Gabriel de Resende <92735087+Lucasgrse@users.noreply.github.com>
2023-02-05 17:52:23 -0300

    Initial commit

---
+++ Acress/Incremental.java
@@ -0,0 +1,11 @@
+class Incremental{
+
+    private static int count = 0;
+    private int numero;
+    public Incremental(){
+        numero = ++count;
+    }
+    public String toString(){
+        return "Incremental" + numero;
+    }
+}
